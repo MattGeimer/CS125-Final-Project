8a4ef773c99419b7e7a56c8bbbe62a847a18c28f
Matt Geimer <matasgeimer@gmail.com>
2019-11-30 09:03:28 -0600

    Added Placeholder Chunk loading
    Chunks now load 50 placeholder playlists and have stubs for all buttons. TODO: Fix buttons not working and connect with spotify/firebase API's

--- app/src/main/java/com/example/playlisterforspotify/MainActivity.java
+++ app/src/main/java/com/example/playlisterforspotify/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.playlisterforspotify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i("MainActivity", "Reached MainActivity.java onCreate");
+
+        LinearLayout playlistList = findViewById(R.id.playlistList);
+
+        //TODO: Replace this with code that fetches playlists
+        for (int i = 0; i < 50; i++) {
+            View playlist = getLayoutInflater().inflate(R.layout.playlist_chunk, playlistList, false);
+            ImageButton playlistCover = playlist.findViewById(R.id.playlistCover);
+            TextView playlistTitle = playlist.findViewById(R.id.playlistTitle);
+            TextView playlistAuthor = playlist.findViewById(R.id.playlistAuthor);
+            ImageButton expand = playlist.findViewById(R.id.expand);
+            ImageButton rateUp = playlist.findViewById(R.id.rateUp);
+            ImageButton rateDown = playlist.findViewById(R.id.rateDown);
+
+            //TODO: Replace with data from fetched playlist
+            playlistTitle.setText("Joe's Playlist No. " + (i + 1));
+            playlistAuthor.setText("Shared by: " + "Joe");
+            playlistCover.setVisibility(View.VISIBLE);
+
+            //TODO: Replace stubs with intended functions of each button
+            expand.setOnClickListener(unused -> {
+                Log.i("Playlist Chunk", "expand button tapped");
+            });
+
+            rateUp.setOnClickListener(unused -> {
+                Log.i("Playlist Chunk", "rateUp button tapped");
+            });
+
+            rateDown.setOnClickListener(unused -> {
+                Log.i("Plailist Chunk", "rateDown button tapped");
+            });
+            playlistList.addView(playlist);
+        }
     }
 }
