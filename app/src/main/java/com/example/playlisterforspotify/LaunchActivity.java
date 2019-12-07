36ef24ba6dfbc095d232c75b4a4a3cc8cd8cdf08
Darius Townes <dtownes2@illinois.edu>
2019-12-07 01:46:32 -0600

    Logging in for the first time will add the user to the database with their Firebase Uid, a child for the ratings they have given, and a child for the playlists they have made.
    Pressing the share button for a user's playlist will add the playlist under shared-playlists, with the key being the playlist id and the value being the overall user rating of the playlist.

--- app/src/main/java/com/example/playlisterforspotify/LaunchActivity.java
+++ app/src/main/java/com/example/playlisterforspotify/LaunchActivity.java
@@ -1,7 +1,6 @@
 package com.example.playlisterforspotify;
 
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.Button;
 
@@ -13,47 +12,11 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import kaaes.spotify.webapi.android.SpotifyApi;
-import kaaes.spotify.webapi.android.SpotifyService;
-import kaaes.spotify.webapi.android.models.PlaylistSimple;
-
 public class LaunchActivity extends AppCompatActivity {
     private static String accessToken;
-    private static String myID;
-
-    private static class GetNewUserData extends AsyncTask<Void, Void, List<String>> {
-        @Override
-        public List<String> doInBackground(Void ... Voids) {
-            SpotifyApi api = new SpotifyApi();
-            api.setAccessToken(accessToken);
-            SpotifyService spotify = api.getService();
-
-            myID = spotify.getMe().id;
-
-            List<String> ownedPlaylistIDs = new ArrayList<>();
-            List<PlaylistSimple> allMyPlaylists = spotify.getMyPlaylists().items;
-            // We only care about the playlists actually owned by the user, not the playlists the user follows.
-            for (PlaylistSimple playlist : allMyPlaylists) {
-                String playlistOwnerID = playlist.owner.id;
-                if (playlistOwnerID.equals(myID)) {
-                    ownedPlaylistIDs.add(playlist.id);
-                }
-            }
-
-            return ownedPlaylistIDs;
-        }
-
-        @Override
-        public void onPostExecute(List<String> playlistIDs) {
-            DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference().child("users");
-            mDatabase.child(FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(myID);
-        }
-    }
-
     /**Request code.*/
     private static final int RC_SIGN_IN = 1;
 
@@ -62,7 +25,6 @@ public class LaunchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
 
-        FirebaseAuth.getInstance().signOut();
         accessToken = getIntent().getStringExtra("accessToken");
 
         Button loginButton = findViewById(R.id.login);
@@ -113,7 +75,14 @@ public class LaunchActivity extends AppCompatActivity {
             IdpResponse response = IdpResponse.fromResultIntent(data);
             if (resultCode == RESULT_OK) {
                 if (response.isNewUser()) {
-                    new GetNewUserData().execute();
+                    String uID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                    DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference().child("users");
+                    DatabaseReference user = mDatabase.child(uID);
+                    DatabaseReference ratings = user.child("ratings");
+                    ratings.child("temp").setValue(0);
+                    DatabaseReference sharedPlaylists = user.child("shared-playlists");
+                    sharedPlaylists.child("temp").setValue(0);
+
                     Intent intent = new Intent(this, MainActivity.class);
                     intent.putExtra("accessToken", accessToken);
                     startActivity(intent);
